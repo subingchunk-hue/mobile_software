08a136a51bceccc20a9b628d624d9e7bbdb2371d
luo319 <2814485297@qq.com>
2025-09-09 10:05:49 +0800

    提交实验6

--- lab6/pages/index/index.js
+++ lab6/pages/index/index.js
@@ -4,7 +4,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    levels:[
+        'level01.png',
+        'level02.png',
+        'level03.png',
+        'level04.png'
+    ]
+  },
+  chooseLevel:function(e){
+      let level=e.currentTarget.dataset.level
+      wx.navigateTo({
+          url:'../game/game?level='+level
+      })
   },
 
   /**
